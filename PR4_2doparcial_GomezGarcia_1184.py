d3b0e5514d9cfd8b20d35c56c1b60381f824f47d
Gomez Garcia Andres Noe <a.23308051281184@cbtis128.edu.mx>
2024-10-17 15:59:39 -0600

    Add files via upload

---
+++ PR4_2doparcial_GomezGarcia_1184.py
@@ -0,0 +1,36 @@
+print(" ")
+print("Andres Noe Gomez Garcia 3°w 1184: practica 4 2do parcial")
+print(" ")
+# Crea un diccionario
+persona = {}
+
+# Pide información al usuario y la añade al diccionario
+nombre =str(input("Introduce el nombre: "))
+persona["Nombre"] = nombre
+print(persona)
+print(" ")
+
+edad =int(input("Introduce la edad: "))
+persona["Edad"] = edad
+print(persona)
+print(" ")
+
+sexo =str(input("Introduce el sexo: "))
+persona["Sexo"] = sexo
+print(persona)
+print(" ")
+
+telefono =int(input("Introduce el teléfono: "))
+persona["Teléfono"] = telefono
+print(persona)
+print(" ")
+
+correo =str(input("Introduce el correo electrónico: "))
+persona["Correo electrónico"] = correo
+print(persona)
+print(" ")
+
+Direccion =str(input("Introduzca su direccion : "))
+persona["Direccion"] = Direccion
+print(persona)
+print(" ")
